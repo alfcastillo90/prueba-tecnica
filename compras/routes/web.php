15fc02b2aa1791b8edd19c4c2daa7048079e8bce
Carlos Alfredo Castillo Rodriguez <alfredo.castillo@lesskomplex.com>
2019-02-22 16:26:16 -0300

    se agregaron las rutas a las tablas

--- compras/routes/web.php
+++ compras/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categorias','CategoriasController');
+Route::resource('compras','ComprasController');
+Route::resource('detalle_compras','DetalleComprasController');
