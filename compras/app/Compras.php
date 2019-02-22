28504c5ee454496fed7a7f1a0c6b5635cec189a9
Carlos Alfredo Castillo Rodriguez <alfredo.castillo@lesskomplex.com>
2019-02-22 16:14:16 -0300

    primer commit, creacion de modelos, migraciones y controladores

---
+++ compras/app/Compras.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Compras extends Model
+{
+    protected $table = 'compras';
+
+    protected $fillable = ['id', 'cantidad', 'producto', 'created_at'];
+}
---
+++ compras/database/migrations/2019_02_22_185649_create_detalle_compras_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDetalleComprasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalle_compras', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->decimal('precio',10,2);
+            $table->integer('compra_id');
+            $table->integer('categoria_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalle_compras');
+    }
+}
